ec95edba460f81a4887995430e5c0b069ca6d359
U-DESKTOP-PFHOM62\RoniePaolo <ronie.arauco@pucp.edu.pe>
2020-08-04 17:10:18 -0500

    First push kafka connect custom smt

---
+++ src/main/java/com/github/jcustenborder/kafka/connect/transform/common/FieldToJSONString.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright © 2017 Jeremy Custenborder (jcustenborder@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jcustenborder.kafka.connect.transform.common;
+
+import com.github.jcustenborder.kafka.connect.utils.config.Description;
+import com.github.jcustenborder.kafka.connect.utils.config.DocumentationTip;
+import com.github.jcustenborder.kafka.connect.utils.config.Title;
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.json.JsonConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class FieldToJSONString<R extends ConnectRecord<R>> extends BaseTransformation<R> {
+    private static final Logger log = LoggerFactory.getLogger(FieldToJSONString.class);
+
+
+    @Override
+    public ConfigDef config() {
+        return FieldToJSONStringConfig.config();
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    JsonConverter converter = new JsonConverter();
+
+    FieldToJSONStringConfig config;
+    Map<Schema, Schema> schemaCache;
+
+    @Override
+    public void configure(Map<String, ?> map) {
+        this.config = new FieldToJSONStringConfig(map);
+        this.schemaCache = new HashMap<>();
+    }
+
+    @Override
+    protected SchemaAndValue processStruct(R record, Schema inputSchema, Struct input) {
+        // Input in Object and Struct fashion
+        final Object inputFieldObject = input.get(this.config.inputFieldName);
+        final Struct inputFieldStruct = input.getStruct(this.config.inputFieldName);
+
+        // Schema
+        final Schema outputSchema = this.schemaCache.computeIfAbsent(inputSchema, s -> {
+            final Field inputField = inputSchema.field(this.config.inputFieldName);
+            final SchemaBuilder builder = SchemaBuilder.struct();
+            for (Field inputField : inputSchema.fields()) {
+                builder.field(inputField.name(), inputField.schema());
+            }
+            builder.field(this.config.inputFieldName, inputField.schema());
+            return builder.build();
+        });
+
+        // Value
+        // Creating outputStruct (all values of struct)
+        final Struct outputStruct = new Struct(outputSchema);
+        for (Field inputField : inputSchema.fields()) {
+            final Object value = input.get(inputField);
+            outputStruct.put(inputField.name(), value);
+        }
+        // Adding the input in JSON string type
+        // Converting to byte buffer
+        final byte[] buffer = this.converter.fromConnectData("dummy", inputFieldStruct.schema(), inputFieldObject);
+        // Encode buffer in UTF_8 string
+        inputFieldValue = new String(buffer, Charsets.UTF_8);
+        // Append the value in outputStruct
+        outputStruct.put(this.config.inputFieldName, inputFieldValue);
+
+        return new SchemaAndValue(outputSchema, outputStruct);
+    }
+
+
+    @Title("FieldToJSONString(Key)")
+    @Description("This transformation is used to extract a field from a nested struct and append it " +
+            "to the parent struct in JSON string.")
+    @DocumentationTip("This transformation is used to manipulate fields in the Key of the record.")
+    public static class Key<R extends ConnectRecord<R>> extends FieldToJSONString<R> {
+
+        @Override
+        public R apply(R r) {
+            final SchemaAndValue transformed = process(r, r.keySchema(), r.key());
+
+            return r.newRecord(
+                    r.topic(),
+                    r.kafkaPartition(),
+                    transformed.schema(),
+                    transformed.value(),
+                    r.valueSchema(),
+                    r.value(),
+                    r.timestamp()
+            );
+        }
+    }
+
+    @Title("FieldToJSONString(Value)")
+    @Description("This transformation is used to extract a field from a nested struct and append it " +
+            "to the parent struct in JSON string.")
+    public static class Value<R extends ConnectRecord<R>> extends FieldToJSONString<R> {
+
+        @Override
+        public R apply(R r) {
+            final SchemaAndValue transformed = process(r, r.valueSchema(), r.value());
+
+            return r.newRecord(
+                    r.topic(),
+                    r.kafkaPartition(),
+                    r.keySchema(),
+                    r.key(),
+                    transformed.schema(),
+                    transformed.value(),
+                    r.timestamp()
+            );
+        }
+    }
+
+}
---
+++ src/main/java/com/github/jcustenborder/kafka/connect/transform/common/FieldToJSONStringConfig.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright © 2017 Jeremy Custenborder (jcustenborder@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jcustenborder.kafka.connect.transform.common;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+
+import java.util.Map;
+
+public class FieldToJSONStringConfig extends AbstractConfig {
+    public final String inputFieldName;
+    public final String outputFieldName;
+
+    public FieldToJSONStringConfig(Map<String, ?> settings) {
+        super(config(), settings);
+        this.inputFieldName = getString(INPUT_FIELD_NAME_CONF);
+        this.outputFieldName = getString(OUTPUT_FIELD_NAME_CONF);
+    }
+
+    public static final String INPUT_FIELD_NAME_CONF = "input.field.name";
+    static final String INPUT_FIELD_NAME_DOC = "The field on the struct to be JSON stringify. "
+    public static final String OUTPUT_FIELD_NAME_CONF = "output.field.name";
+    static final String OUTPUT_FIELD_NAME_DOC = "The field to place the JSON stringify value into.";
+
+    public static ConfigDef config() {
+        return new ConfigDef()
+                .define(INPUT_FIELD_NAME_CONF, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, INPUT_FIELD_NAME_DOC)
+                .define(OUTPUT_FIELD_NAME_CONF, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, OUTPUT_FIELD_NAME_DOC);
+    }
+
+}
