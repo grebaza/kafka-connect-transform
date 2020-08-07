c74d14a4545c3b40dfecce7d040e391a52c17f28
Guillermo Rebaza <grebaza@gmail.com>
2020-08-07 14:39:26 -0500

    new config w/comma-separated spec

--- src/main/java/com/github/jcustenborder/kafka/connect/transform/common/FieldToJSONStringConfig.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/transform/common/FieldToJSONStringConfig.java
@@ -29,21 +29,21 @@ public class FieldToJSONStringConfig extends AbstractConfig {
   public final boolean schemasEnable;
   public final Schema.Type outputSchemaType;
 
-  public static final String INPUT_FIELD_NAME_CONFIG = "input.field.name";
-  static final String INPUT_FIELD_NAME_DOC = "The field on the struct to be JSON stringify. ";
-  public static final String OUTPUT_FIELD_NAME_CONFIG = "output.field.name";
-  static final String OUTPUT_FIELD_NAME_DOC = "The field to place the JSON stringify value into.";
+  public static final String SPEC_CONFIG = "input.field.name";
+  public static final String SPEC_DOC = "The field on the struct to be JSON" +
+    " stringify. ";
   public static final String SCHEMAS_ENABLE_CONFIG = "schemas.enable";
-  public static final String SCHEMAS_ENABLE_DOC = "Flag to determine if the JSON data should include the schema.";
-  public static final String OUTPUT_SCHEMA_CONFIG = "output.schema.type";
-  public static final String OUTPUT_SCHEMA_DOC = "The connect schema type to output the converted JSON as.";
+  public static final String SCHEMAS_ENABLE_DOC = "Flag to determine if the" +
+    " JSON data should include the schema.";
 
   public FieldToJSONStringConfig(Map<String, ?> settings) {
     super(config(), settings);
     this.inputFieldName = getString(INPUT_FIELD_NAME_CONFIG);
     this.outputFieldName = getString(OUTPUT_FIELD_NAME_CONFIG);
     this.schemasEnable = getBoolean(SCHEMAS_ENABLE_CONFIG);
-    this.outputSchemaType = ConfigUtils.getEnum(Schema.Type.class, this, OUTPUT_SCHEMA_CONFIG);
+    this.outputSchemaType = ConfigUtils.getEnum(
+        Schema.Type.class, this, OUTPUT_SCHEMA_CONFIG
+        );
   }
 
 
@@ -51,30 +51,31 @@ public class FieldToJSONStringConfig extends AbstractConfig {
 
     return new ConfigDef()
       .define(
-          INPUT_FIELD_NAME_CONFIG, ConfigDef.Type.STRING,
-          ConfigDef.Importance.HIGH, INPUT_FIELD_NAME_DOC
-       )
-      .define(
-          OUTPUT_FIELD_NAME_CONFIG, ConfigDef.Type.STRING,
-          ConfigDef.Importance.HIGH, OUTPUT_FIELD_NAME_DOC
-       )
-      .define(
         SCHEMAS_ENABLE_CONFIG, ConfigDef.Type.BOOLEAN, false,
         ConfigDef.Importance.MEDIUM, SCHEMAS_ENABLE_DOC
        )
-      .define(
-          ConfigKeyBuilder.of(OUTPUT_SCHEMA_CONFIG, ConfigDef.Type.STRING)
-          .documentation(OUTPUT_SCHEMA_DOC)
-          .defaultValue(Schema.Type.STRING.toString())
-          .validator(
-            ConfigDef.ValidString.in(
-              Schema.Type.STRING.toString(),
-              Schema.Type.BYTES.toString()
-              )
-            )
-          .importance(ConfigDef.Importance.MEDIUM)
-          .build()
-       );
+    .define(SPEC_CONFIG, ConfigDef.Type.LIST, ConfigDef.NO_DEFAULT_VALUE,
+        new ConfigDef.Validator() {
+          @SuppressWarnings("unchecked")
+          @Override
+          public void ensureValid(String name, Object valueObject) {
+            List<String> value = (List<String>) valueObject;
+            if (value == null || value.isEmpty()) {
+              throw new ConfigException("Must specify at least one field to cast.");
+            }
+            parseFieldTypes(value);
+          }
+
+          @Override
+          public String toString() {
+            return "list of colon-delimited pairs, e.g. " +
+              "<code>foo:bar:baz,abc:rst:xyz</code>";
+          }
+        },
+        ConfigDef.Importance.HIGH,
+        "List of fields,schema type, and new fields' names " +
+        "field1:schema-type:new-field1,field1:schema-type:new-field1."
+        );
   }
 
 }
